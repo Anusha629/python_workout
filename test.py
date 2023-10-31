a83fd58f0c23e968f0cd43469c323a1d3b1b23b4
anusha <anushakizhakkeveedu@gmail.com>
2023-10-31 17:54:53 +0530

    Added test code for occurance of a string

--- test.py
+++ test.py
@@ -21,3 +21,31 @@ if __name__ == '__main__':
     unittest.main()
     
     
+    
+def freq(s):
+    freq_dict = {}
+    for letter in s:
+        if letter in freq_dict:
+            freq_dict[letter] += 1
+        else:
+            freq_dict[letter] = 1
+    return freq_dict
+
+class TestFreq(unittest.TestCase):
+
+    def testIsFreq(self):
+        sentence = "hello galaxy"
+        self.assertTrue(freq(sentence))
+
+    def testCaseInsensitive(self):         
+         sentence = "Hello GAlaxy"
+         self.assertTrue(freq(sentence))
+
+    def testEmptyInput(self):
+        ret = freq("")
+        self.assertFalse(ret)
+        
+
+if __name__ == '__main__':
+    unittest.main()
+
