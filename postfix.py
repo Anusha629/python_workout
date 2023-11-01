46fe4f1402f8b24338a99918737f74c7b9e79b56
anusha <anushakizhakkeveedu@gmail.com>
2023-11-01 16:49:57 +0530

    GREEN: Test passed for addition of two numbers

--- postfix.py
+++ postfix.py
@@ -2,7 +2,10 @@ def evaluate(exp):
     stack = []
     for i in exp:
         if i in ['+']:
-            pass
+            num_2 = stack.pop()
+            num_1 = stack.pop()
+            if i == "+":
+                stack.append(num_1 + num_2)
         else:
             stack.append(int(i))
     return stack.pop()
