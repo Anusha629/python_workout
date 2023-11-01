be40606381943aad7a3ca5a7bf2ccc98b4fc5e65
anusha <anushakizhakkeveedu@gmail.com>
2023-11-01 17:17:33 +0530

    GREEN: subtraction of two numbers

--- postfix.py
+++ postfix.py
@@ -1,11 +1,13 @@
 def evaluate(exp):
     stack = []
     for i in exp:
-        if i in ['+']:
+        if i in ['+','-']:
             num_2 = stack.pop()
             num_1 = stack.pop()
             if i == "+":
                 stack.append(num_1 + num_2)
+            elif i == "-":
+                stack.append(num_1 - num_2)
         else:
             stack.append(int(i))
     return stack.pop()
